2214538feec3cfefc0bfccc636eebba9979c7882
vnazarko <alg.vnazarenko@gmail.com>
2023-09-05 18:11:48 +0300

    made a header block

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
-
+import Header from "./components/Header.vue"
+export default {
+  components: {
+    Header,
+  }
+}
 </script>
 
 <template>
-  
+  <div class="wrapper">
+    <Header/>
+  </div>
 </template>
 
 <style scoped>
