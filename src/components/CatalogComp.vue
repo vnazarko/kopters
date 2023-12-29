2e053dc4abe92b8b8c34eb3e0a1007e0f3275e7a
vnazarko <alg.vnazarenko@gmail.com>
2023-12-29 12:56:48 +0300

    изменение маршрута

--- src/components/CatalogComp.vue
+++ src/components/CatalogComp.vue
@@ -8,7 +8,7 @@ export default {
             {
                 bg: kopter,
                 title: 'Потолочные вентиляторы',
-                path: '/catalog/vent'
+                path: '/vent'
             },
           ]
         }
--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,7 @@ const router = createRouter({
       component: CatalogView
     },
     {
-      path: '/catalog/vent',
+      path: '/vent',
       name: 'vent',
       component: ProductView
     }
